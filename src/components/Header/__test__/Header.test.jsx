a244673ce33882f44d8271546479b7bfd30a3027
Akash Sharma <lucky1612000@gmail.com>
2022-10-08 19:16:04 +0530

    written test for header

---
+++ src/components/Header/__test__/Header.test.jsx
@@ -0,0 +1,33 @@
+import { render, screen } from '@testing-library/react';
+import Header from '../Header';
+
+it('should render the correct heading based in text', () => {
+  render(<Header title={'A header'} />);
+  let headingElement = screen.getByText(/a header/i);
+  expect(headingElement).toBeInTheDocument();
+});
+
+it('should render the correct heading based on role', () => {
+  render(<Header title={'A header'} />);
+  let headingElement = screen.getByRole('heading');
+  expect(headingElement).toBeInTheDocument();
+});
+
+it('should render the correct heading using findByRole', async () => {
+  render(<Header title={'A header'} />);
+  let headingElement = await screen.findByRole('heading');
+  expect(headingElement).toBeInTheDocument();
+});
+
+
+it('should render the correct heading using queryByRole', () => {
+  render(<Header title={'A header'} />);
+  let headingElement = screen.queryByText(/cats/i);
+  expect(headingElement).not.toBeInTheDocument();
+});
+
+it('should render the correct heading using getAllByRole', () => {
+  render(<Header title={'A header'} />);
+  let headingElement = screen.getAllByRole('heading');
+  expect(headingElement.length).toBe(1);
+});
